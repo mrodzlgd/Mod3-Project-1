e76f56d85c597cb427008cc293d4a5d3da17bfa4
peter-haferl <peter.haferl@gmail.com>
2019-08-26 16:36:51 -0400

    lint visualzations script

--- visualizations.py
+++ visualizations.py
@@ -8,19 +8,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import hypothesis_tests as ht
 
-def plot_distribution(sample_one,label_one,sample_two,label_two,title):
-    plt.figure(figsize=(14,7))
+
+def plot_distribution(sample_one, label_one, sample_two, label_two, title):
+    '''Plot distributions given data and specifications'''
+    plt.figure(figsize=(14, 7))
     sns.distplot(sample_one, label=label_one)
     sns.distplot(sample_two, label=label_two)
     plt.legend(loc=0)
     plt.xlabel(title)
     plt.title(title)
-    
+
+
 def visualization_four(cleaned_data):
-    
+    '''Plot barplot of average effect size by diagnosis group'''
     cum_codes = ht.hypothesis_test_four(cleaned_data)
-    
-    plt.figure(figsize=(14,7))
+    plt.figure(figsize=(14, 7))
     sns.barplot('code', 'avg_effect', data=cum_codes, palette='deep')
     plt.xticks(rotation=90)
     plt.ylabel('Average Effect')
