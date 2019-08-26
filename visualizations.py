53832d3ad1696926bd9462c3392fc4fcc3b03842
peter-haferl <peter.haferl@gmail.com>
2019-08-26 16:30:41 -0400

    edit visualization and create technical notebook

--- visualizations.py
+++ visualizations.py
@@ -6,87 +6,22 @@ A framework for each type of visualization is provided.
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import hypothesis_tests as ht
 
-# Set specific parameters for the visualizations
-large = 22; med = 16; small = 12
-params = {'axes.titlesize': large,
-          'legend.fontsize': med,
-          'figure.figsize': (16, 10),
-          'axes.labelsize': med,
-          'xtick.labelsize': med,
-          'ytick.labelsize': med,
-          'figure.titlesize': large}
-plt.rcParams.update(params)
-plt.style.use('seaborn-whitegrid')
-sns.set_style("white")
-
-
-def overlapping_density(package=None, input_vars=None, target_var=None):
-    """
-    Set the characteristics of your overlapping density plot
-    All arguments are set to None purely as a filler right now
-
-    Function takes package name, input variables(categories), and target variable as input.
-    Returns a figure
-
-    Should be able to call this function in later visualization code.
-
-    PARAMETERS
-
-    :param package:        should only take sns or matplotlib as inputs, any other value should throw and error
-    :param input_vars:     should take the x variables/categories you want to plot
-    :param target_vars:    the y variable of your plot, what you are comparing
-    :return:               fig to be enhanced in subsequent visualization functions
-    """
-
-    # Set size of figure
-    fig = plt.figure(figsize=(16, 10), dpi=80)
-
-    # Starter code for figuring out which package to use
-    if package == 'sns':
-        for variable in input_vars:
-            sns.kdeplot(variable[target_var])
-    elif package == 'matplotlib':
-        for variable in input_vars:
-            plt.plot(..., label=None, linewidth=None, color=None, figure = fig)
-
-    return fig
-
-
-def visualization_one(target_var = None, input_vars= None, output_image_name=None):
-    """
-    The visualization functions are what is used to create each individual image.
-    The function should be repeatable if not generalizable
-    The function will call either the boxplot or density plot functions you wrote above
-
-    :param target_var:
-    :param input_vars:
-    :param output_image_name: the desired name for the image saved
-    :return: outputs a saved png file and returns a fig object for testing
-    """
-    ###
-    # Main chunk of code here
-    ###
-    overlapping_density(package='sns', input_vars, target_vars)
-
-    # Starter code for labeling the image
-    plt.xlabel(None, figure = fig)
-    plt.ylabel(None, figure = fig)
-    plt.title(None, figure= fig)
-    plt.legend()
-
-    # exporting the image to the img folder
-    plt.savefig(f'img/{output_image_name}.png', transparent = True, figure = fig)
-    return fig
-
-
-# please fully flesh out this function to meet same specifications of visualization one
-
-def visualization_two(output_image_name):
-    pass
-
-def visualization_three(output_image_name):
-    pass
-
-def visualization_four(output_image_name):
-    pass
+def plot_distribution(sample_one,label_one,sample_two,label_two,title):
+    plt.figure(figsize=(14,7))
+    sns.distplot(sample_one, label=label_one)
+    sns.distplot(sample_two, label=label_two)
+    plt.legend(loc=0)
+    plt.xlabel(title)
+    plt.title(title)
+    
+def visualization_four(cleaned_data):
+    
+    cum_codes = ht.hypothesis_test_four(cleaned_data)
+    
+    plt.figure(figsize=(14,7))
+    sns.barplot('code', 'avg_effect', data=cum_codes, palette='deep')
+    plt.xticks(rotation=90)
+    plt.ylabel('Average Effect')
+    plt.xlabel('Diagnosis Group')
